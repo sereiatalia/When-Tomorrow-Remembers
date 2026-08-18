5bf776f55669a3e1cbf1606d7105449a8edb9b60
Aly Guevarra <alysajuliengvrr@gmail.com>
2026-08-18 17:36:38 +0800

    Configure automatic GitHub Pages publishing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: process.env.GITHUB_PAGES === "true" ? "export" : undefined,
+  assetPrefix:
+    process.env.GITHUB_PAGES === "true" ? "/When-Tomorrow-Remembers" : undefined,
 };
 
 export default nextConfig;
